0ca608bf422eff380312ce3d64d81a552b6fbfc9
Chancy <chancyleath@hotmail.com>
2018-08-30 13:31:11 -0600

    initial files created and puesdocoded

---
+++ assets/javascript/logic.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+
+    //intial array of gif topids to populate in buttons
+    var topics = ["irritated", "laughing", "happy", "sad", "angry", "eye roll"];
+
+    //need to make buttons in js with the values from the array
+
+    //load those buttons to page
+
+    
+    //api key for giphy
+    var apiKey = "api_key=BBccJQM11fXinnCUWdUrWfK9kpo2qWPq&";
+
+   //on click call the giphy api
+
+    //need to input value from button as the "q" in the url when calling
+
+    //display gifs in dom, with the rating
+
+    //need to make new buttons based on user input and add to page
+
+    //when user clicks on those, needs to call the giphy api and display with rating
+
+    //do a clear button to reset the dom/remove added buttons
+
+
+
+
+
+
+
+
+
+
+})
